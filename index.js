a19db1e4d9a062d768f7d0905133df9e7e01bad0
Annu-cs-123 <65020183+Annu-cs-123@users.noreply.github.com>
2021-03-20 13:36:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,131 +1,127 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3000;
-const mongodb = require('mongodb');
-const mongodbClient = mongodb.MongoClient;
+const port = process.env.PORT || 9900;
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-
-const mongoURL = 'mongodb+srv://annu:annu@123@cluster0.fxcgy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-let db; 
-
+const mongourl = mongodb+srv://annu:annu@123@cluster0.fxcgy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+let db;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-mongodbClient.connect(mongoURL,{ useUnifiedTopology: true }, (error, connection)=>{
-    if(error) throw error;
-    db = connection.db('internshipprogram')
-    app.listen(port,(error)=>{
-        if(error) throw error;
-    })
-})
+//health Check
+app.get('/',(req,res) => {
+    res.send("Health Ok");
+});
 
-
-
-//for health check up
-app.get('/',(req,res)=>{
-    res.status(200).send("health ok ok");
-})
-
-
-app.get('/city', function(req, res) {
-    let sortcondition = { city_name: 1 }
-    let limit = 100;
-    if (req.query.sort && req.query.limit) {
-        sortcondition = { city_name: Number(req.query.sort) },
-            limit = Number(req.query.limit)
-    } else if (req.query.sort) {
-        sortcondition = { city_name: Number(req.query.sort) }
-    } else if (req.query.limit) {
-        limit = Number(req.query.limit)
+//city Route
+app.get('/city',(req,res) => {
+    let sortcondition = {city_name:1};
+    let limit =100
+    if(req.query.sort && req.query.limit ){
+      sortcondition = {city_name:Number(req.query.sort)};
+      limit =Number(req.query.limit)
     }
-    db.collection('city').find().sort(sortcondition).limit(limit).toArray((err, result) => {
-        if (err) throw err;
-        res.send(result)
-    })
-})
-
-//////rest route/////// 
-app.get('/rest', (req, res) => {
-    var condition = {};
-    ///sort data basis of cost// 
-   let sortcondition = {cost:1} ;
-   if(req.query.mealtype && req.query.sort){
-       condition = {"type.mealtype":req.query.mealtype}
-       sortcondition = {cost:Number(req.query.sort)}; 
-   } 
-    //meal+city
-    if (req.query.mealtype && req.query.city) {
-        condition = { $and: [{ "type.mealtype": req.query.mealtype }, { city: req.query.city }] }
+    else if(req.query.sort){
+      sortcondition = {city_name:Number(req.query.sort)}
+    }else if(req.query.limit){
+      limit =Number(req.query.limit)
     }
-    //meal+cuision
-    else if (req.query.mealtype && req.query.cuision) {
-        condition = { $and: [{ "type.mealtype": req.query.mealtype }, { "Cuisine.cuisine": req.query.cuision }] }
+    db.collection('city').find().sort(sortcondition).limit(limit).toArray((err,result) => {
+      if(err) throw err;
+      res.send(result);
+    })
+   
+});
+
+//rest details
+app.get('/rest/:id',(req,res) =>{
+  var id = req.params.id
+  db.collection('restaurent').find({_id:id}).toArray((err,result) => {
+    if(err) throw err;
+    res.send(result)
+  })
+})
+
+//Rest route
+app.get('/rest',(req,res) => {
+  var condition ={};
+  let sortcondition = {cost:1};
+  if(req.query.mealtype && req.query.sort){
+    condition={"type.mealtype":req.query.mealtype}
+    sortcondition= {cost:Number(req.query.sort)};
+  }
+    //meal +cost
+    if(req.query.mealtype && req.query.lcost && req.query.hcost){
+      condition={$and:[{"type.mealtype":req.query.mealtype},{cost:{$lt:Number(req.query.hcost),$gt:Number(req.query.lcost)}}]}
     }
-    //meal+cost 
-    else if (req.query.mealtype && req.query.lcost && req.query.hcost) {
-        condition = {
-            $and: [{ "type.mealtype": req.query.mealtype }, { cost: { $lt: Number(req.query.hcost), $gt: Number(req.query.lcost) } }]
-        }
-    } 
-    //city//
-    else if (req.query.city) {
-        condition = { city: req.query.city }
+    //meal +city
+    else if(req.query.mealtype && req.query.city){
+      condition={$and:[{"type.mealtype":req.query.mealtype},{city:req.query.city}]}
     }
-    // mealtype 
-    else if (req.query.mealtype) {
-        condition = { "type.mealtype": req.query.mealtype }
+    //meal +cuisine
+    else if(req.query.mealtype && req.query.cuisine){
+      condition={$and:[{"type.mealtype":req.query.mealtype},{"Cuisine.cuisine":req.query.cuisine}]}
     }
-    db.collection('restaurant').find(condition).sort(sortcondition).toArray((err, result) => {
-        if (err) throw err;
-        // res.status(200).send(result)
-        res.send(result)
-    })
+   
+    //meal
+    else if(req.query.mealtype){
+      condition={"type.mealtype":req.query.mealtype}
+    }
+    //city
+    else if(req.query.city){
+      condition={city:req.query.city}
+    }
+  db.collection('restaurent').find(condition).sort(sortcondition).toArray((err,result)=>{
+    if(err) throw err;
+    res.send(result)
+  }) 
 })
 
-//////rest parem
-app.get('/rest/:id', (req,res) => {
-    var id = req.params.id
-    db.collection('restaurant').find({_id:id}).toArray((err,result) => {
-        if (err) throw err;
-        res.send(result)
-    })
+//MealType Route
+app.get('/meal',(req,res) => {
+  db.collection('mealType').find().toArray((err,result) => {
+    if(err) throw err;
+    res.send(result)
+  })
+})
+
+//cuisine route
+app.get('/cuisine',(req,res) => {
+  db.collection('cuisine').find().toArray((err,result) => {
+    if(err) throw err;
+    res.send(result)
+  })
+})
+
+//placeorder
+app.post('/placeorder',(req,res)=>{
+  db.collection('orders').insert(req.body,(err,result) => {
+    if(err) throw err;
+    res.send('data added');
+  })
+})
+
+//get all bookings
+app.get('/orders',(req,res) => {
+  db.collection('orders').find({}).toArray((err,result) => {
+    if(err) throw err;
+    res.send(result)
+  })
 })
 
 
+//connection with mongo serer
+MongoClient.connect(mongourl,(err,connection) => {
+  if(err) console.log(err);
+  db = connection.db('internshipprogram');
 
-
-////cuision route//////
-app.get('/cusion', (req, res) => {
-    db.collection('cuision').find().toArray((err, result) => {
-        if (err) throw err;
-        res.send(result)
-    })
-})
-
-//// mealtype route//////
-app.get('/mealtype', (req, res) => {
-    db.collection('mealType').find().toArray(function(err, result) {
-        if (err) throw err;
-        res.send(result)
-    })
-})
-
-///placeorder 
-app.post('/placeholder', (req, res) => {
-    db.collection('orders').insert(req.body, (err, result) => {
-        if (err) throw err;
-        res.send("data added")
-    })
-})
-
-////get booking 
-app.get('/order', (req, res) => {
-    db.collection('orders').find({}).toArray((err, result) => {
-        if (err) throw err;
-        res.send(result)
-    })
+  app.listen(port,(err) => {
+    if(err) throw err;
+    console.log(`Server is running on port ${port}`)
+  })
 })
